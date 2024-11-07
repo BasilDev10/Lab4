da3d820bc7efbfa577b7889d18aa82a4738b9968
Basil <basilaltasan@gmail.com>
2024-11-07 10:56:57 +0300

    upload 2

--- src/Main.java
+++ src/Main.java
@@ -9,32 +9,33 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         do {
-            System.out.println("type calculation you want : ");
+            System.out.println("type calculation of Shape you want : ");
             System.out.println("1. Circle\n2. Rectangle\n3. Triangle\n4. Exit");
             int choice = input.nextInt();
+            double radius,height,base = 0;
             switch (choice) {
                 case 1:
                     System.out.println("Enter Radius:");
-                    double radius = input.nextDouble();
+                    radius = input.nextDouble();
                     Circle circle = new Circle(radius);
                     System.out.println("The area of the circle is " + String.format("%.2f",circle.calculateArea()));
                     System.out.println("The circumference of the circle is " + String.format("%.2f",circle.calculateCircumference()));
                     break;
                 case 2:
                     System.out.println("Enter Height:");
-                    double height = input.nextDouble();
+                    height = input.nextDouble();
                     System.out.println("Enter Base:");
-                    double base = input.nextDouble();
+                    base = input.nextDouble();
                     Rectangle rectangle = new Rectangle(height, base);
                     System.out.println("The area of the rectangle is " + String.format("%.2f",rectangle.calculateArea()));
                     System.out.println("The circumference of the rectangle is " + String.format("%.2f",rectangle.calculateCircumference()));
                     break;
                 case 3:
                     System.out.println("Enter Height:");
-                    double height1 = input.nextDouble();
+                    height = input.nextDouble();
                     System.out.println("Enter Base:");
-                    double base1 = input.nextDouble();
-                    Triangle triangle = new Triangle(height1, base1);
+                    base = input.nextDouble();
+                    Triangle triangle = new Triangle(height, base);
                     System.out.println("The area of the triangle is " + String.format("%.2f",triangle.calculateArea()));
                     System.out.println("The circumference of the triangle is " +  String.format("%.2f", triangle.calculateCircumference()));
                     break;
